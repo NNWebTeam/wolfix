b71fa71fb9cca5e3118c675e3f19f55daf07cb59
mnycz@pgs-soft.com <mnycz@pgs-soft.com>
2017-03-03 19:24:51 +0100

    Corected hex hover in partners component

--- src/app/routes/Partnerzy/Partnerzy.js
+++ src/app/routes/Partnerzy/Partnerzy.js
@@ -14,6 +14,7 @@ import image9 from './../../assets/photos/filamentyPLA/filament (9).jpg'
 import image10 from './../../assets/photos/filamentyPLA/filament (10).jpg'
 import image11 from './../../assets/photos/filamentyPLA/filament (11).jpg'
 import image12 from './../../assets/photos/filamentyPLA/filament (12).jpg'
+import hexHover from './../../assets/hex.png'
 
 const Partnerzy = ({
 	firstPartner,
@@ -27,7 +28,7 @@ const Partnerzy = ({
 }) =>
 	<div className="partnerzy">
 		<div className="main-hex">
-			<div className="hex">
+			<div className="hex" style={hexStyle}>
 				{text}
 			</div>
 		</div>
@@ -35,76 +36,81 @@ const Partnerzy = ({
 			<div className="partners-hex partner1"onClick={() => insertHexToMainHex(firstPartner)}>
 				<div className="rotation">
 					<Image src={image1}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner2" onClick={() => insertHexToMainHex(secondPartner)}>
 				<div className="rotation">
 					<Image src={image2}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner3" onClick={() => insertHexToMainHex(thirdPartner)}>
 				<div className="rotation">
 					<Image src={image3}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner4" onClick={() => insertHexToMainHex(fourthPartner)}>
 				<div className="rotation">
 					<Image src={image4}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner5" onClick={() => insertHexToMainHex(fifthPartner)}>
 				<div className="rotation">
 					<Image src={image5}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner6" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image6}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner7" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image7}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner8" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image8}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner9" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image9}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner10" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image10}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner11" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image11}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>
 			<div className="partners-hex partner12" onClick={() => insertHexToMainHex(sixthPartner)}>
 				<div className="rotation">
 					<Image src={image12}/>
-					<div className="hover"></div>
+					<div className="hover" style={hexStyle}></div>
 				</div>
 			</div>			
 		</div>
 	</div>
 
 export default Partnerzy
+
+const hexStyle = {
+	backgroundImage: `url(${hexHover})`,
+	backgroundSize: '100%'
+}
